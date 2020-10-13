721869bc733c5d919ff40e119a08ffcfd020b86b
Samuel Trahan <samtrahan91@gmail.com>
2020-10-12 20:20:33 -0500

    nav bar and logo add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from './components/NavBar/NavBar'
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <NavBar />
+    </>
   )
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './NavBar.css'
+
+export default function NavBar() {
+  return (
+    <>
+      <nav className="nav-bar">
+        <div>
+          <a href="/"><img alt="" src="/images/ibeatlogan.png"></img></a>
+        </div>
+      </nav>
+    </>
+  )
+}
+
