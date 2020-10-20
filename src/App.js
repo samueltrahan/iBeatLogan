66fc27ac205c32820373b74fea3d0c317415e3d4
Samuel Trahan <samtrahan91@gmail.com>
2020-10-20 17:47:48 -0500

    leader board components and styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AddGame from './components/AddGame/AddGame';
 import NavBar from './components/NavBar/NavBar';
 import './App.css';
+import axios from 'axios';
+import LeaderBoard from './components/LeaderBoard/LeaderBoard';
 
 export default function App() {
+  const [horse, setHorse] = useState([]);
+  const [oneOnOne, setOneOnOne] = useState([]);
+  const [twentyOne, setTwentyOne] = useState([]);
+
+  useEffect(() => {
+    const handleLeaderBoard = async () => {
+      const response = await axios.get('/leaderBoard');
+      setHorse(response.data.horse.sort((a, b) => b.wins - a.wins));
+      setOneOnOne(response.data.oneOnOne.sort((a, b) => b.wins - a.wins));
+      setTwentyOne(response.data.twentyOne.sort((a, b) => b.wins - a.wins));
+    };
+    handleLeaderBoard();
+  }, []);
+
   return (
-    <Router>
-      <NavBar />
-      <Route exact path="/addgame" render={() => <AddGame />} />
-    </Router>
+    <>
+      <Router>
+        <NavBar />
+        <Route exact path="/addgame" render={() => <AddGame />} />
+      </Router>
+      <LeaderBoard horse={horse} oneOnOne={oneOnOne} twentyOne={twentyOne} />
+    </>
   );
 }
---
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './LeaderBoard.css';
+
+export default function LeaderBoard({ horse, oneOnOne, twentyOne }) {
+  return (
+    <>
+      <h1 className="leader-board">Leader Board</h1>
+      <h1 className="twenty-one">21</h1>
+      {twentyOne.map(twenty => (
+        <li className="name-wins">
+          {twenty.name} - {twenty.wins}
+        </li>
+      ))}
+      <h1 className="one-v-one">1 v. 1</h1>
+      {oneOnOne.map(onevone => (
+        <li className="name-wins">
+          {onevone.name} - {onevone.wins}
+        </li>
+      ))}
+      <h1 className="horse">HORSE</h1>
+      {horse.map(h => (
+        <li className="name-wins">
+          {h.name} - {h.wins}
+        </li>
+      ))}
+    </>
+  );
+}
