655431a8f8dc8c1dc4f20217f321172818507cb5
JP Angelle <jp@angelle.me>
2020-10-21 11:12:12 -0500

    add POST endpoint for add-result

--- functions/leaderboards/leaderboards.js
+++ functions/leaderboards/leaderboards.js
@@ -7,20 +7,52 @@ const faunaClient = new faunadb.Client({
 
 const q = faunadb.query;
 
-exports.handler = async () => {
+exports.handler = async event => {
+  // checks to ensure this request is a POST
+  if (event.httpMethod !== 'GET') {
+    return {
+      body: 'Method Not Allowed. GET route only.',
+      statusCode: 405,
+    };
+  }
+
   try {
-    const req = await faunaClient.query(
+    // gets the player data from the horse collection
+    const horseData = await faunaClient.query(
       q.Map(
         q.Paginate(q.Documents(q.Collection('horse'))),
         q.Lambda(x => q.Get(x)),
       ),
     );
 
-    const horse = req.data.map(player => player.data);
+    // gets the player data from the twentyOne collection
+    const twentyOneData = await faunaClient.query(
+      q.Map(
+        q.Paginate(q.Documents(q.Collection('twentyOne'))),
+        q.Lambda(x => q.Get(x)),
+      ),
+    );
+
+    // gets the player data from the oneOnOne collection
+    const oneOnOneData = await faunaClient.query(
+      q.Map(
+        q.Paginate(q.Documents(q.Collection('oneOnOne'))),
+        q.Lambda(x => q.Get(x)),
+      ),
+    );
+
+    // maps for the just the player data and excludes unnecessary database data
+    const horse = horseData.data.map(player => player.data);
+    const twentyOne = twentyOneData.data.map(player => player.data);
+    const oneOnOne = oneOnOneData.data.map(player => player.data);
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ horse, oneOnOne: [], twentyOne: [] }),
+      body: JSON.stringify({
+        horse,
+        oneOnOne,
+        twentyOne,
+      }),
     };
   } catch (err) {
     return {
