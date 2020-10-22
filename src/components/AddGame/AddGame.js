8f5f95cb3960035a9b680b344a507976722266ab
JP Angelle <jp@angelle.me>
2020-10-22 07:57:15 -0500

    feat: add some basic form validation (#16)

--- src/components/AddGame/AddGame.js
+++ src/components/AddGame/AddGame.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import GamesDropDown from '../GamePage/GamesDropDown';
@@ -7,11 +7,14 @@ import PickPlayerDD from '../GamePage/PickPlayerDD';
 
 import './AddGame.css';
 
+const isANumber = value => !Number.isNaN(Number(value));
+
 export default function AddGame() {
   const history = useHistory();
   const [player, setPlayer] = useState('');
   const [game, setGame] = useState('');
-  const [wins, setWins] = useState();
+  const [wins, setWins] = useState('');
+  const [disabled, setDisabled] = useState(true);
 
   const handleFormSubmit = async event => {
     event.preventDefault();
@@ -22,14 +25,24 @@ export default function AddGame() {
     });
     history.push('/');
   };
+
+  useEffect(() => {
+    if (player && game && isANumber(wins)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  }, [game, player, wins]);
+
   return (
     <>
       <h1 className="heading">Add a Game</h1>
       <form>
         <PickPlayerDD setPlayer={setPlayer} />
         <GamesDropDown setGame={setGame} />
-        <DidYouWin setWins={setWins} />
+        <DidYouWin setWins={setWins} wins={wins} />
         <button
+          disabled={disabled}
           className="negative ui button"
           type="submit"
           onClick={handleFormSubmit}
