29164e92b91c2d00aeddaab35040f3615eaf1b95
JP Angelle <jp@angelle.me>
2020-10-26 21:37:37 -0500

    feat: add empty state component

--- src/components/LeaderBoards/Leaderboard.js
+++ src/components/LeaderBoards/Leaderboard.js
@@ -1,14 +1,26 @@
+import { EmptyLeaderboard } from './EmptyLeaderboard';
 import { NAMES_DISPLAY_NAME } from '../../constants';
 
-export const Leaderboard = ({ players }) => (
-  <div className="leaderboard">
-    {players.map(player => (
+export const Leaderboard = ({ players }) => {
+  if (players.length) {
+    return (
       <>
-        <span className="leaderboard-name">
-          {NAMES_DISPLAY_NAME[player.name]}
-        </span>
-        <span className="leaderboard-wins">{player.wins}</span>
+        <div className="leaderboard-header">
+          <span>Player</span>
+          <span>Wins</span>
+        </div>
+        <div className="leaderboard">
+          {players.map(player => (
+            <>
+              <span className="leaderboard-name">
+                {NAMES_DISPLAY_NAME[player.name]}
+              </span>
+              <span className="leaderboard-wins">{player.wins}</span>
+            </>
+          ))}
+        </div>
       </>
-    ))}
-  </div>
-);
+    );
+  }
+  return <EmptyLeaderboard />;
+};
